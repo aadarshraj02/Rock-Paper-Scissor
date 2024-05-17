408c200cf76ad3bec96f1725f4c7390eecb522b5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-17 21:09:31 +0530

    added a function playGame to take user choice

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let userScore = 0;
 let compScore = 0;
 
 let choices = document.querySelectorAll(".choice");
+const playGame = (userChoice) => {
+  console.log("choice clicked", userChoice);
+};
 
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
-    console.log("choice clicekd", userChoice);
   });
 });
