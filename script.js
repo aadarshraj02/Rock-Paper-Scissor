bf472424fc64ba503a178be5f2839dc18cdda50d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-17 21:32:06 +0530

    applied condition for game draw

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let userScore = 0;
 let compScore = 0;
 
 let choices = document.querySelectorAll(".choice");
+
 const playGame = (userChoice) => {
   console.log("choice clicked", userChoice);
   const compChoice = generateComputerChoice();
   console.log(compChoice);
+
+  if (userChoice === compChoice) {
+    drawGame();
+  }
 };
 
 const generateComputerChoice = () => {
